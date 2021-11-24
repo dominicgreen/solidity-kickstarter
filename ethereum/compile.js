f1685869022d7fb3d7e9f72a83f068c2ba80222d
Dominic Green <dominic@avantarte.com>
2021-11-24 18:29:58 +0000

    Completed contract build function
    Split out campaign factory contract and campaign into build dir

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -2,4 +2,26 @@ const path = require('path');
 const solc = require('solc');
 const fs = require('fs-extra');
 
-ss
+const buildPath = path.resolve(__dirname,'build');
+fs.removeSync(buildPath);
+
+//get campaign contract path
+const campaignPath = path.resolve(__dirname,'contracts', 'Campaign.sol');
+
+//Get campaign contract
+const source = fs.readFileSync(campaignPath,'utf8');
+
+//Compile campaign contract
+const output = solc.compile(source,1).contracts;
+
+//check and created build folder
+fs.ensureDirSync(buildPath);
+
+//Get each contract and write them to build
+for(let contract in output){
+    const fileName = `${contract.replace(':','')}.json`;
+    fs.outputJsonSync(
+        path.resolve(buildPath,fileName),
+        output[contract]
+    );
+}
