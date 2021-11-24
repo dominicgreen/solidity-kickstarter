2db3f21263be51b1ce11859625250c57bca45f04
Dominic Green <dominic@avantarte.com>
2021-11-24 19:11:45 +0000

    Setup test campaign

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -14,9 +14,26 @@ let campaign;
 beforEach(async () => {
     accounts = await web3.eth.getAccounts();
 
-    //Get the factory contract
-    //Deploy the contract from account 0
+    //Get the factory contract from ABI
+    //Deploy the factory from account 0
     factory = await new web3.rth.Contract(Json.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
         .send({ from: accounts[0], gas: '1000000' });
+
+    //Create campaign with minimum contribution of 100 wei
+    await factory.methods.createCampaign('100').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    //get first element of campaigns and assign to campaign address
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
+
+    //Get a campaign from interfact and campaign address
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
+
+
 });
