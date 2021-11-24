9194e1a3ff3c9325cfee2fe5d3434edf85c284de
Dominic Green <dominic@avantarte.com>
2021-11-24 19:18:45 +0000

    Fixed gnahce reference and added first test

--- test/campaign.test.js
+++ test/campaign.test.js
@@ -1,29 +1,30 @@
-const assert = require('assert');
-const ganache = require('gnache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(gnache.provider());
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-const compiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
+const { isTypedArray } = require("util/types");
 
 let accounts;
 let factory;
 let campaignAddress;
 let campaign;
 
-beforEach(async () => {
+beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
 
     //Get the factory contract from ABI
     //Deploy the factory from account 0
-    factory = await new web3.rth.Contract(Json.parse(compiledFactory.interface))
+    factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({ data: compiledFactory.bytecode })
-        .send({ from: accounts[0], gas: '1000000' });
+        .send({ from: accounts[0], gas: "1000000" });
 
     //Create campaign with minimum contribution of 100 wei
-    await factory.methods.createCampaign('100').send({
+    await factory.methods.createCampaign("100").send({
         from: accounts[0],
-        gas: '1000000'
+        gas: "1000000",
     });
 
     //get first element of campaigns and assign to campaign address
@@ -34,6 +35,11 @@ beforEach(async () => {
         JSON.parse(compiledCampaign.interface),
         campaignAddress
     );
+});
 
-
+describe("Campaigns", () => {
+    it("deploys a factory and a campaign", () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
 });
