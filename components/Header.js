d711c4eb1e28e2e44ad93c879a2601e6fb8509ec
Dominic Green <dominic@avantarte.com>
2021-12-03 09:58:11 +0000

    Wrap up styling

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
-
 const Header = (props) => (
+
+
     <Menu style={{ marginTop: '10px' }}>
         <Menu.Item name="Home">Home</Menu.Item>
         <Menu.Menu position="right">
--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Header from "./Header"
+import Head from 'next/head';
 import { Container } from 'semantic-ui-react';
 const Layout = (props) => (
     <Container>
+        <Head>
+            <link
+                rel="stylesheet"
+                href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+            ></link>
+        </Head>
         <Header />
         {props.children}
     </Container>
