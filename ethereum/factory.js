79e322cf169e3e6c939faf8d05a5371dc60bcb33
Dominic Green <dominic@avantarte.com>
2021-12-07 14:24:27 +0000

    Update build script

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x3E8707c9FA8c859D94BC8A8e810df4c7cBBEaFF3'
+  '0x9B0d475D6cb63D03DD9C06e56988413e3D4C2d3c'
 );
 
 export default instance;
